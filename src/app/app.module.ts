4bcce949143f72c6d6b583d8f111d4e0c317b063
kuhaku001 <jorge.fj.carcamo@gmail.com>
2023-06-19 13:04:06 -0300

    creando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
+import { PaguinaNoEncontradaComponent } from './paguina-no-encontrada/paguina-no-encontrada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    InicioComponent,
+    MenuComponent,
+    PaguinaNoEncontradaComponent
   ],
   imports: [
     BrowserModule,
