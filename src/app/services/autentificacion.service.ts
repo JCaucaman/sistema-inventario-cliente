30411a0042503d70d3c8ec74a0cdb9630184a67d
kuhaku001 <jorge.fj.carcamo@gmail.com>
2023-06-24 11:50:14 -0300

    peticiones http material

--- src/app/services/autentificacion.service.ts
+++ src/app/services/autentificacion.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +9,15 @@ export class AutentificacionService {
   private URL = 'http://localhost:3000'
 
   constructor(
-    private http: HttpClient,
-    private Router : Router
+    private http: HttpClient
     ) { }
 
-  login(user: object) {
-    return this.http.post<any>(this.URL + '/', user);
+  login(usuario: object) {
+    return this.http.post<any>(this.URL + '/', usuario); 
   }
 
   loggedIn() : Boolean{
-    if(localStorage.getItem('token')){
+    if(localStorage.getItem('token')){ // cambiar
       return true
     } else {
       return false
