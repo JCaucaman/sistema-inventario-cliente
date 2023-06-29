b85820a6c1956c7ebb07b4c2b6845e7de8409bf7
kuhaku001 <jorge.fj.carcamo@gmail.com>
2023-06-29 16:34:38 -0300

    Eliminar 1 material ✅

--- src/app/components/Inventario-All/materiales/materiales.component.ts
+++ src/app/components/Inventario-All/materiales/materiales.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MaterialService } from "../../../services/material.service";
 import { CompartirService } from "../../../services/compartir.service";
 
@@ -8,6 +8,8 @@ import { CompartirService } from "../../../services/compartir.service";
   styleUrls: ['./materiales.component.css']
 })
 export class MaterialesComponent implements OnInit {
+
+  id: string = ''
 
   constructor(
     private MaterialService : MaterialService,
@@ -28,8 +30,44 @@ export class MaterialesComponent implements OnInit {
     )
   }
 
-  contextmenu(event: any){
+  // Mouse
 
+  @ViewChild('menu') menu! :ElementRef
+
+  contextmenu(event: MouseEvent){
+    event.preventDefault();
+
+    this.menu.nativeElement.style.display = "block";
+    this.menu.nativeElement.style.top = event.pageY + "px"
+    this.menu.nativeElement.style.left = event.pageX + "px"
+
+    const target = event.target as HTMLElement;
+
+    this.id = target.id.split('-')[0]
+
+    console.log(this.id)
+
+  }
+
+  click(event: MouseEvent){
+    this.menu.nativeElement.style.display = "none";
+  }
+
+  disappearContext(){
+    this.menu.nativeElement.style.display = "none";
+  }
+
+  eliminarMaterial(){
+    this.MaterialService.materialEliminar(this.id)
+    .subscribe(
+      res => {
+        const newMateriales = this.CompartirService.materiales.filter(
+          (material: any) => material._id !== this.id
+        )
+        this.CompartirService.materiales = newMateriales
+      },
+      err => console.log(err)
+    )
   }
 
 }
