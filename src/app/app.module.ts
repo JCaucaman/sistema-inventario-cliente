848bf873cd67217ef89e662c028d8ff8e0c74032
Nacforr <nicolascalistovargas@gmail.com>
2023-07-06 17:13:46 -0300

    toast removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -56,7 +56,6 @@ import { ModalAgregarEtiquetaComponent } from './components/Inventario-All/modal
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot(),
   ],
   providers: [
     AutenticacionGuard,
