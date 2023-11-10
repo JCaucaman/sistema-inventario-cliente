331e552d4c52211fe26cf853274d04e46424d0d5
lucas <lucas.gallardo.calisto@.com>
2023-11-10 05:09:43 -0300

    el login centrado y el css

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,22 +7,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-event: any;
-onUsernameChange(_event: any) {
-  this.username = _event.target.value;
-throw new Error('Method not implemented.');
-}
-  username: string | undefined;
-  password: string | undefined;
-  newUsername: string | undefined;
-  newPassword: string | undefined;
-  error: string | undefined;
+  event: any;
+  username: string = '';
+  password: string = '';
+  newUsername: string = '';
+  newPassword: string = '';
+  error: string = '';
 
   constructor(private router: Router) {}
 
   login() {
-    if (this.username === 'usuario' && this.password === 'contrasena') {
-      this.router.navigate(['/pagina']); // aqui te redirige a la pagina de inicio que esta en proceso chicos.
+    if (this.username === 'usuario' && this.password === 'contraseña') {
+      this.router.navigate(['/pagina']); // Aquí te redirige a la página de inicio que está en proceso.
     } else {
       this.error = 'Usuario o contraseña incorrectos';
     }
@@ -30,16 +26,15 @@ throw new Error('Method not implemented.');
 
   register() {
     if (this.newUsername && this.newPassword) {
-      // Aquí estaba pensando en implementar la lógica para registrar nuevos usuarios y los que ya existen claro y guardarlos en la base de datos que les parece, esta parte la pueden implementar ustedes ya que el mongodb no me funciona.
+      // Lógica para registrar nuevos usuarios y guardarlos en la base de datos
       if (this.newUsername === 'usuario_existente') {
         this.error = 'El nombre de usuario ya está en uso, por favor, elige otro.';
       } else {
-
-        // Aquí lo mismo debería realizar guardado en la base de datos.
+        // Aquí deberia realizar el guardado en la base de datos.
         this.error = 'Registro exitoso, redirigiendo a la página de inicio de sesión...';
         setTimeout(() => {
-          this.router.navigate(['/pagina']); // aqui iria la pagina de inicio de sesion, ya que te redirige a iniciar sesion con tu cuenta recien creada, pummmm.
-        }, 2000); // estos 2 mil son 2 segundos osea que al crear tu cuenta te redirige a los 2 segundos xd no se asusten.
+          this.router.navigate(['/pagina']); // Aquí iría la página de inicio de sesión.
+        }, 2000); // Espera de 2 segundos antes de redirigir.
       }
     } else {
       this.error = 'Por favor, completa todos los campos para registrarte.';
